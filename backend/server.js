8f830e568aa153fce69fc8856c8f50ee5415d0a8
Kumar Aditya <adityaakumar3004@gmail.com>
2026-07-24 18:38:21 +0530

    Prepare Backend for production deployment

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,15 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 dotenv.config({
-  path: path.join(__dirname, ".env"),
+    path: path.join(__dirname, ".env"),
 });
 
-console.log("Loaded .env from:", path.join(__dirname, ".env"));
-console.log(process.env);
-
-require("./config/db"); // Connect to MySQL
+require("./config/db");
 
 const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on port ${PORT}`);
 });
