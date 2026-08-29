e6970a6528066a1cf2ede7709d44857c7e4ef260
Kumar Aditya <adityaakumar3004@gmail.com>
2026-08-29 19:22:09 +0530

    Update frontend API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://procureease-backend-om74.onrender.com/api",
 });
 
 api.interceptors.request.use(
